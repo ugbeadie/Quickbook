a9cb7045029b47fe920358690c4314e275a51193
ugbeadie <ugbeadie3@gmail.com>
2025-11-17 20:53:59 +0100

    trying to resolve timezone issue on vercel

--- app/api/transactions/category/route.ts
+++ app/api/transactions/category/route.ts
@@ -1,7 +1,34 @@
-// app/api/transactions/category/route.ts
+// // app/api/transactions/category/route.ts
+
+// import { NextResponse } from "next/server";
+// import { getTransactionsCategory } from "@/lib/actions";
+
+// export async function POST(req: Request) {
+//   try {
+//     const body = await req.json();
+
+//     const result = await getTransactionsCategory(
+//       body.category,
+//       body.type,
+//       body.period,
+//       new Date(body.currentDate)
+//     );
+
+//     return NextResponse.json(result);
+//   } catch (error) {
+//     console.error("API ERROR (category):", error);
+//     return NextResponse.json(
+//       { error: "Failed to load category transactions" },
+//       { status: 500 }
+//     );
+//   }
+// }
 
 import { NextResponse } from "next/server";
 import { getTransactionsCategory } from "@/lib/actions";
+
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export async function POST(req: Request) {
   try {
